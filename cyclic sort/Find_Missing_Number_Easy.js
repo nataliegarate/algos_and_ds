fe235979fc73b400b72cd1af5940eb2348934fa3
nataliegarate <natalieegarate@hotmail.com>
2020-02-11 11:25:10 -0800

    find misssing number

---
+++ cyclic sort/Find_Missing_Number_Easy.js
@@ -0,0 +1,26 @@
+function find_missing_number(nums) {
+    let i = 0;
+    const n = nums.length;
+    while (i < n) {
+      j = nums[i];
+      if (nums[i] < n && nums[i] !== nums[j]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]]; // swap
+      } else {
+        i += 1;
+      }
+          console.log(nums)
+  
+    }
+  
+    // find the first number missing from its index, that will be our required number
+    for (i = 0; i < n; i++) {
+      if (nums[i] !== i) {
+        return i;
+      }
+    }
+  
+    return n;
+  }
+  
+  //NOTE: the largest one will end up in the place of the missing number
+  console.log(find_missing_number([4, 0, 3, 1]));
