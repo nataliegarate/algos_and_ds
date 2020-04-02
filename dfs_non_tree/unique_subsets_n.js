8f440ef13728c7db658e28583247dbba9645b796
nataliegarate <natalieegarate@hotmail.com>
2020-04-01 18:23:43 -0700

    name changes

---
+++ dfs_non_tree/unique_subsets_n.js
@@ -0,0 +1,19 @@
+function unique_subsets(nums, n) {
+  let results = [];
+  let temp = [];
+
+  function helper(nums, n, idx) {
+    if (temp.length === n) {
+      results.push(temp.slice());
+    }
+    for (let i = idx; i < nums.length; i++) {
+      temp.push(nums[i]);
+      helper(nums, n, i + 1);
+      temp.pop();
+    }
+  }
+  helper(numys, n, 0);
+  return results;
+}
+let numys = [1, 2, 3];
+console.log(unique_subsets(numys, 2));
