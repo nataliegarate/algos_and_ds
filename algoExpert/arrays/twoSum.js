3fac10624af8af74145a6900f8a8e0b333377842
nataliegarate <natalieegarate@hotmail.com>
2020-04-11 17:26:29 -0700

    new prob

--- algoExpert/arrays/twoSum.js
+++ algoExpert/arrays/twoSum.js
@@ -1,18 +1,36 @@
+// function twoNumberSum(array, targetSum) {
+//   array = array.sort((a, b) => a - b);
+//   let results = [];
+//   let num1Idx = 0;
+//   let num2Idx = array.length - 1;
+//   while (array[num1Idx] < array[num2Idx]) {
+//     console.log(array[num1Idx], array[num2Idx]);
+//     if (array[num1Idx] + array[num2Idx] === targetSum) {
+//       results.push([array[num1Idx], array[num2Idx]]);
+//       num1Idx++;
+//       num2Idx--;
+//     } else if (array[num1Idx] + array[num2Idx] < targetSum) {
+//       num1Idx++;
+//     } else {
+//       num2Idx--;
+//     }
+//   }
+//   return results;
+// }
+
 function twoNumberSum(array, targetSum) {
-  array = array.sort((a, b) => a - b);
   let results = [];
-  let num1Idx = 0;
-  let num2Idx = array.length - 1;
-  while (array[num1Idx] < array[num2Idx]) {
-    console.log(array[num1Idx], array[num2Idx]);
-    if (array[num1Idx] + array[num2Idx] === targetSum) {
-      results.push([array[num1Idx], array[num2Idx]]);
-      num1Idx++;
-      num2Idx--;
-    } else if (array[num1Idx] + array[num2Idx] < targetSum) {
-      num1Idx++;
-    } else {
-      num2Idx--;
+  let numMap = {};
+  array.forEach((num) => (numMap[num] = num));
+  for (let i = 0; i < array.length; i++) {
+    let neededNumber = targetSum - array[i];
+    if (
+      typeof numMap[neededNumber] === "number" &&
+      numMap[neededNumber] !== array[i]
+    ) {
+      numMap[neededNumber] = "used";
+      numMap[array[i]] = "used";
+      results.push(neededNumber, array[i]);
     }
   }
   return results;
