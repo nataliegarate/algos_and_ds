f0a49974239cad7559aade92deaf6f4c6bfad257
nataliegarate <natalieegarate@hotmail.com>
2020-05-01 19:09:04 -0700

    linked list q

---
+++ interview_questions/linked_list_pattern.js
@@ -0,0 +1,33 @@
+function reOrderLinkedList(head) {
+    let length = getLength(head);
+    halfLength = Math.max(length);
+    let list2Head = //get head for list 2
+        let list1 = // returns head and sets last to null
+            list2Head = reverseList(list2Head);
+    mergeLists(list1, list2);
+    return head;
+}
+
+function getLength(node) {
+    //this gets length of LL
+}
+
+function merge(list1, list2) {
+    //merges two lists
+}
+
+function makeListOne(node, halfLength) {
+    // first List
+}
+
+function getListTwoHead(node, halfLength) {
+    // return listTwo Head
+}
+
+function reverseList(node) {
+    //reverse the list
+}
+
+function mergeLists(list1, list2) {
+    //merges them
+}
