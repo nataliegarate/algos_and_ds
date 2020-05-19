84d3c6eb448e5598bb87ecfd09db102cd4469f82
nataliegarate <natalieegarate@hotmail.com>
2020-05-19 15:12:01 -0700

    has subtree

---
+++ misc_binary_trees/has_subtree.js
@@ -0,0 +1,30 @@
+var isSubtree = function (s, t) {
+  if (s === null) {
+    return false;
+  }
+  let result = false;
+  if (s.val === t.val) {
+    result = compareTrees(s, t);
+  }
+  if (result) {
+    return true;
+  } else {
+    return isSubtree(s.left, t) || isSubtree(s.right, t);
+  }
+};
+
+function compareTrees(node1, node2) {
+  if (!node1 && !node2) {
+    return true;
+  }
+  if (!node1 || !node2) {
+    return false;
+  }
+  if (node1.val !== node2.val) {
+    return false;
+  }
+  let self = node1.val === node2.val;
+  let left = compareTrees(node1.left, node2.left);
+  let right = compareTrees(node1.right, node2.right);
+  return left && right && self;
+}
